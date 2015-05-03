0849db47d6492d7bb6a50792fa0c14fa7da9cc41
Karthik Ram <karthik.ram@gmail.com>
2015-05-03 15:16:40 -0700

    Cleaned up code a bit

--- code.R
+++ code.R
@@ -5,14 +5,24 @@ library(wesanderson)
 # This is an example query that gets 1000 observations
 x <- ee_observations(genus = "vulpes", georeferenced = TRUE)
 # Get unique species so we can add colors
-unique_species <- x$data %>% group_by(scientific_name) %>% summarise(n = n()) %>% arrange(desc(n))
+unique_species <- x$data %>% 
+						group_by(scientific_name) %>% 
+						summarise(n = n()) %>% arrange(desc(n))
 pal <- wes_palette("Zissou", 100, type = "continuous")
 colors <- pal[1:nrow(unique_species)]
+# I need some variation here
+colors[1:5] <- ("#c0392b", "#27ae60", "#f1c40f", "#3498db", "#3498db")
 unique_species$marker_color <- colors
 
+
 filtered_df <- left_join(x$data, unique_species, by = "scientific_name")
-filtered_df <- filtered_df %>% select(scientific_name, begin_date, marker_color, latitude, longitude)
+filtered_df <- filtered_df %>% 
+				select("title" = scientific_name, 
+					"description" = begin_date, 
+					"marker-color" = marker_color, 
+					latitude, longitude)
 
 head(filtered_df)
 library(geojsonio)
+# Why does geojson_write change marker-color to marker.color?
 geojson_write(filtered_df, lat = "latitude", lon = "longitude", file = "points.geojson")
