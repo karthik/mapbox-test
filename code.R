260e199df45643ef0329681f611b61252a4bca40
Karthik Ram <karthik.ram@gmail.com>
2015-05-03 16:03:37 -0700

    A super janky prototype of a mapbox function works

--- code.R
+++ code.R
@@ -2,27 +2,45 @@ library(ecoengine)
 library(dplyr)
 library(wesanderson)
 
-# This is an example query that gets 1000 observations
-x <- ee_observations(genus = "vulpes", georeferenced = TRUE)
-# Get unique species so we can add colors
-unique_species <- x$data %>% 
-						group_by(scientific_name) %>% 
+# SUPER JANKY EMBARRASING MAP FUNCTION
+# REPLACEMENT FOR ee_map()
+ee_map2 <- function(eco) {
+df <- eco$data
+unique_species <- df %>%
+						group_by(scientific_name) %>%
 						summarise(n = n()) %>% arrange(desc(n))
 pal <- wes_palette("Zissou", 100, type = "continuous")
 colors <- pal[1:nrow(unique_species)]
-# I need some variation here
-colors[1:5] <- ("#c0392b", "#27ae60", "#f1c40f", "#3498db", "#3498db")
+# I need some variation here, goddammit
+colors[1:5] <- c("#c0392b","#27ae60", "#f1c40f", "#3498db", "#3498db")
 unique_species$marker_color <- colors
 
-
-filtered_df <- left_join(x$data, unique_species, by = "scientific_name")
-filtered_df <- filtered_df %>% 
-				select("title" = scientific_name, 
-					"description" = begin_date, 
-					"marker-color" = marker_color, 
+# Remove all the extra fields and only keep what goes in the geoJSON
+filtered_df <- left_join(df, unique_species, by = "scientific_name")
+filtered_df <- filtered_df %>%
+				select("title" = scientific_name,
+					"description" = begin_date,
+					"marker-color" = marker_color,  # THis is for mapbox
 					latitude, longitude)
+filtered_df$`marker-size` <- "medium"
 
-head(filtered_df)
 library(geojsonio)
 # Why does geojson_write change marker-color to marker.color?
 geojson_write(filtered_df, lat = "latitude", lon = "longitude", file = "points.geojson")
+system("sed 's/marker.color/marker-color/' points.geojson > f1.geojson")
+system("sed 's/marker.size/marker-size/' f1.geojson > f2.geojson")
+system("cat index0.html f2.geojson index1.html > index.html")
+file.remove('f1.geojson')
+file.remove('f2.geojson')
+file.remove('points.geojson')
+browseURL('index.html')
+}
+
+# Other examples
+
+ee_observations(genus = "Lynx", georeferenced = TRUE) %>% ee_map2
+
+
+
+
+
